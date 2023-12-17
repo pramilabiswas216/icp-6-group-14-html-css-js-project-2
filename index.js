e74ab4f18cc02cc578835f56f0cab5043bb7c1b6
NikitDale12 <nikitadale12aug@gmail.com>
2023-12-17 05:57:04 -0800

    add contact

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function value(){
     let navbar = document.getElementById("navbar");
     navbar.style.backgroundColor="black";
 }
+
+
+function changeImage(){
+    let displayImage = document.getElementById("img1")
+    if(displayImage.src.match("./image1.png")){
+        displayImage.src = '.././image2.png'
+    } else{
+        displayImage.src = "./image4.png"
+    } 
+    
+}
